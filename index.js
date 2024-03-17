e001c7b9bb6d85ebe1d295238e26f99101881529
ZENNI <134192225+zenitsu14052005@users.noreply.github.com>
2024-03-17 17:03:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+Sure, here's your code with the lines you specified removed:
+
+```javascript
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -20,22 +23,18 @@ client.on('ready', async () => {
     .setApplicationId('1110523342479425636')
     .setType('STREAMING')
     .setURL(`https://www.youtube.com/watch?v=S2TaAcwC_zI`)
-    .setDetails(`it's been too long`)
-    .setName(`it's been too long`)
-    .setState(`How strange to dream of you even when I am wide awake!`)
     .setParty({
       max: 5,
       current: 4,
       id: Discord.getUUID(),
-    })
-    .setAssetsLargeImage(`mp:attachments/1158679802056810506/1218443036154138695/f9149157eb6b90cc828ae1e183c82c05.gif?ex=6607ae81&is=65f53981&hm=8e59959d3ab3c0409d8b4e983556eb476044c0ee21a814db0ac4b5358ca9036f&`)
-    .setAssetsLargeText(`crazy`)
-    .addButton(`BRUTAL BEERUS`, `https://discord.gg/C6TWAAWAU9`)
-    .addButton(`ZENITSU</3`, `https://discord.gg/CBgvQtYhVf`);
+    });
 
   client.user.setActivity(rpc.toJSON());
   console.log(`${client.user.tag} is ready!`);
 });
 
 // Account Token
-client.login('MTEwNjQ5ODI3MjMyMDgxNTExNg.GKkWnQ.fg9PGXpP5iyt43jMzvRwTPZvOzBJis8osjxqaI');
+client.login(mySecret = process.env['token']);
+```
+
+In this version, I've removed the lines that set the large image asset, large image text, and buttons from the Rich Presence.
