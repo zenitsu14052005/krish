cf29b11bdbada9437404bdcb8039a6e353b444ba
ZENNI <134192225+zenitsu14052005@users.noreply.github.com>
2024-03-17 16:52:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Self Bot Rich Presence working');
+});
+app.listen(process.env.PORT, () => {
+  console.log('Self Bot Rich Presence working on port ' + process.env.PORT);
+});
+
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+  checkUpdate: false
+});
+
+client.on('ready', async () => {
+  const rpc = new Discord.RichPresence({
+    startTimestamp: Date.now()
+  })
+    .setApplicationId('1110523342479425636')
+    .setType('STREAMING')
+    .setURL(`https://www.youtube.com/watch?v=S2TaAcwC_zI`)
+    .setDetails(`it's been too long`)
+    .setName(`it's been too long`)
+    .setState(`How strange to dream of you even when I am wide awake!`)
+    .setParty({
+      max: 5,
+      current: 4,
+      id: Discord.getUUID(),
+    })
+    .setAssetsLargeImage(`mp:attachments/1158679802056810506/1218443036154138695/f9149157eb6b90cc828ae1e183c82c05.gif?ex=6607ae81&is=65f53981&hm=8e59959d3ab3c0409d8b4e983556eb476044c0ee21a814db0ac4b5358ca9036f&`)
+    .setAssetsLargeText(`crazy`)
+    .addButton(`BRUTAL BEERUS`, `https://discord.gg/C6TWAAWAU9`)
+    .addButton(`ZENITSU</3`, `https://discord.gg/CBgvQtYhVf`);
+
+  client.user.setActivity(rpc.toJSON());
+  console.log(`${client.user.tag} is ready!`);
+});
+
+// Account Token
+client.login('MTEwNjQ5ODI3MjMyMDgxNTExNg.GKkWnQ.fg9PGXpP5iyt43jMzvRwTPZvOzBJis8osjxqaI');
